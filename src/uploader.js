cba77e12c036390a4ce1aa5e9da11c43ae19c596
Tim Hatch <hatch.tim@gmail.com>
2022-02-13 12:38:41 +0000

    Adopt use-file-upload
    Use the use-file-upload package for simplified upload handling

--- src/uploader.js
+++ src/uploader.js
@@ -1,71 +1,40 @@
-import React, {useRef, useState}  from "react"
-import axios                      from 'axios'
+import React, {useState}  from "react"
+import axios              from 'axios'
 
-import Chip                       from '@material-ui/core/Chip'
-import Button                     from '@material-ui/core/Button'
-import Snackbar                   from '@material-ui/core/Snackbar'
+import {useFileUpload}    from 'use-file-upload'
+
+import Chip               from '@material-ui/core/Chip'
+import Snackbar           from '@material-ui/core/Snackbar'
 
 const BASE_URL = process.env.REACT_APP_API_ENDPOINT
-const UI_DELAY = 2000
+const UI_DELAY = 5000
 
-function UploadForm({url}) {
-  const [selectedFile, setSelectedFile] = useState(null)
-  const [message, setMessage]           = useState(null)
+function UploadFiles({url}) {
+  const [file, selectFile]    = useFileUpload()
+  const [message, setMessage] = useState(null)
 
-  const submitForm = () => {
+  const handleSelect = () => {
     const formData = new FormData()
-    formData.append("file", selectedFile)
-    // console.log(selectedFile)
-    axios
-      .post(BASE_URL + url, formData)
-      .then((res) => { 
-        setMessage('file uploaded')
-        setTimeout(() => setSelectedFile(null), UI_DELAY)
-      })        
-      .catch((err) => {
-        setMessage('file upload error')
-        setTimeout(() => setSelectedFile(null), UI_DELAY)
-      }) 
+    selectFile({accept: 'application/json'}, ({source, name, size, file}) => {
+      console.log({source, name, size, file})
+      formData.append("file", file)
+      axios.post(BASE_URL + url, formData)
+           .then((res) => setMessage('upload success'))
+           .catch((err) => setMessage('upload failed'))
+
+    })
   }
 
-  const cancelForm = () => setSelectedFile(null)
-
-  // TODO: Disable the buttons if no file is selected
   return (
-    <div style={{width: '20rem', backgroundColor: 'azure', padding: '2rem'}}>
-      <form>
-        <FileUploader selectedFile={selectedFile} setSelectedFile={setSelectedFile} />
-        <Button variant="outlined" onClick={submitForm} style={{marginRight: '2rem'}}>submit</Button>
-        <Button variant="outlined" onClick={cancelForm}le={{marginRight: '2rem'}}>cancel</Button>
-      </form>
+    <div style={{backgroundColor: 'azure', padding: '2rem'}}>
+      <Chip label='upload file' style={{width: '6rem'}} onClick={handleSelect} />
+      <span style={{marginLeft: '1rem', fontSize: 'small'}}>{file ? file.name : 'no file selected'}</span>
       <ConnectionStatusMessage {...{message, setMessage}} />
-    </div>
+    </div>    
   )
 }
 
-export default UploadForm
-
-// Uploader component
-function FileUploader({selectedFile, setSelectedFile}) {
-  const fileInput = useRef(null)
-  const filename  = (selectedFile || {name: 'no file selected'}).name
-
-  const handleClick = (e) => fileInput.current.click()
-
-  const handleChange = (e) => {
-    // console.log(isJSON(e.target.files[0]))
-    setSelectedFile(e.target.files[0])
-  }
-
-  return (
-    <div style={{margin: '0.5rem 0'}}>
-      <input type="file" onChange={handleChange} ref={fileInput} style={{display: 'none'}}/>
-      <Chip label='choose file' style={{width: '8rem'}} color='secondary' onClick={handleClick} />
-      <span style={{marginLeft: '1rem', fontSize: 'small'}}>{filename}</span>
-    </div>
-  )
-}
-
+export default UploadFiles
 
 // Snackbar to indicate send success/failure
 function ConnectionStatusMessage({message, setMessage}) {
@@ -75,7 +44,3 @@ function ConnectionStatusMessage({message, setMessage}) {
     <Snackbar open={displayBar} autoHideDuration={UI_DELAY} onClose={handleClose} message={message} />
   )
 }
-
-// function isJSON(file) {
-//   return file.type === 'application/json' ? true : false
-// }
