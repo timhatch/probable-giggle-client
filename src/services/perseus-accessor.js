18a6617c096d5179a739089d1cfa8d740f40b740
Tim Hatch <hatch.tim@gmail.com>
2022-02-26 16:54:44 +0000

    Update .env variables
    Simplify development and production variables

--- src/services/perseus-accessor.js
+++ src/services/perseus-accessor.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 // Default URL (defined in (.env.development and .env.production)
-const results_url = process.env.REACT_APP_RESULTS_ENDPOINT
-const session_url = process.env.REACT_APP_SESSION_ENDPOINT
+const BASE_URL    = process.env.REACT_APP_API_ENDPOINT 
 
 // Fetch data from Perseus
 // - axios defaults to { type: 'json' } so don't specify this
@@ -12,17 +11,17 @@ const session_url = process.env.REACT_APP_SESSION_ENDPOINT
 //   but axios-cancel appears to remove requewstIds when complete, so concurrently
 //   running requests are cancelled
 //
-const fetch = async (query) => axios.get(results_url, { 
+const fetch = async (query) => axios.get(`${BASE_URL}/results`, { 
   params: { time: new Date().getHours(), ...query },
   requestId: query.type || JSON.stringify(query)
 })
 
-const update = async (data) => axios.put(`${results_url}/person`, data)
+const update = async (data) => axios.put(`${BASE_URL}/results/person`, data)
 
 // Class definition
 class PerseusHTTPService {
   static async fetchDefaults() {
-    let response = await axios.get(session_url)
+    let response = await axios.get(`${BASE_URL}/session`)
     console.log(response.data)
   }
 
@@ -30,10 +29,10 @@ class PerseusHTTPService {
   // @params = comp (WetId), cat (GrpId) and route (route)
   static async fetchResults(query) {
     const uuid   = `${query.cat}.${query.route}`
-    const params = { wet_id: query.comp, grp_id: query.cat, route: query.route }
+    const params = {wet_id: query.comp, grp_id: query.cat, route: query.route}
     try {
       let response = await fetch(params)
-      return { [uuid]: response.data.map((x) => ({ ...x, ...params })) }
+      return {[uuid]: response.data.map((x) => ({...x, ...params}))}
     } catch (e) {
       return null
     }
@@ -53,7 +52,7 @@ class PerseusHTTPService {
   // @params = :wet_id, :grp_id, :route, :locked, [:per_id]
   static async updateLockState(params) {
     try {
-      const response = await axios.put(`${results_url}/lock`, params)
+      const response = await axios.put(`${BASE_URL}/results/lock`, params)
       return response
     } catch (e) {
       return {status: 500}
@@ -65,7 +64,7 @@ class PerseusHTTPService {
   // @params = :wet_id, :grp_id, :route, [:per_id]
   static async delete(action, params) {
     try {
-      const response = await axios.put(`${results_url}/${action}`, params)
+      const response = await axios.put(`${BASE_URL}/results/${action}`, params)
       return response
     } catch (e) {
       return {status: 500}
