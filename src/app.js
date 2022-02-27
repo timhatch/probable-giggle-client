3f7cdd916b2a2d5d87012f7c98f7b145ddaf9a8c
Tim Hatch <hatch.tim@gmail.com>
2022-02-27 13:03:27 +0000

    Update app.js
    Re-jig the home page

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
 import React, {Fragment}  from 'react'
+import styled             from 'styled-components'
 
 import {Link}             from 'react-router-dom'
 
 import Button             from '@material-ui/core/Button'
 
+const VFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 10rem;
+  margin: 1rem 0;
+`
+
+const Container = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 function Home() {
   return (
-    <Fragment>
-      <main>
-        <h2>Options</h2>
-      </main>
-      <nav>
-        <h3>Competition Administration</h3>
-        <Button variant="outlined" style={{width: '12rem'}}>
-          <Link to="/admin"  style={{textDecoration: 'none'}}>Administration</Link>
-        </Button>
+    <Container>
+      <VFlex>
         <h3>Results Editors</h3>
         <Button variant="outlined"  style={{width: '12rem'}}>
           <Link to="/desktop" style={{textDecoration: 'none'}}>Desktop Scorer</Link>
@@ -23,8 +36,14 @@ function Home() {
         <Button variant="outlined" style={{width: '12rem'}}>
           <Link to="/mobile"  style={{textDecoration: 'none'}}>Tablet Scorer</Link>
         </Button>
-      </nav>
-    </Fragment>
+      </VFlex>
+      <VFlex>
+        <h3>Administration</h3>
+        <Button variant="outlined" style={{width: '12rem'}}>
+          <Link to="/admin"  style={{textDecoration: 'none'}}>Administration</Link>
+        </Button>
+      </VFlex>
+    </Container>
   )
 }
 
