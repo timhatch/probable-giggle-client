23c7bf11dd4c135db25efcff794b90c5c45f6d4f
Tim Hatch <hatch.tim@gmail.com>
2022-02-28 20:34:34 +0000

    Update index.js
    Incorporate mobile CSS rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React          from 'react';
 import ReactDOM       from 'react-dom';
 
-import {Provider}     from 'mobx-react'
-
 import Home           from './app'
 import ScorerTablets  from './scorer-tablets'
 import ScorerDesktop  from './scorer-desktop'
@@ -11,12 +9,15 @@ import Administration from './administration'
 import {HashRouter}     from "react-router-dom";
 import {Routes, Route}  from "react-router-dom";
 
+import {Provider}     from 'mobx-react'
+
 import Store          from './store' 
 import HTTPService    from './services/perseus-accessor'
 
 import './typography.css'
 import './react-data-grid.css'
 import './index.css'
+import './index-mobile.css'
 
 const store = new Store(HTTPService, { storageKey: 'test' })
 
